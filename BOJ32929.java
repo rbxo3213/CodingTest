b31d51eac04163e7b6b327c25e6e5f248c2babb9
rbxo3213 <42289726+rbxo3213@users.noreply.github.com>
2025-12-30 20:37:33 +0900

    BOJ32929.java 만들기

---
+++ BOJ32929.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = "UOS";
+        String[] line = str.split("");
+        int n = sc.nextInt();
+        
+        System.out.println(line[(n - 1) % 3]);
+}
+
+}
